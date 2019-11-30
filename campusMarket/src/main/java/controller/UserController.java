90f77379a4a2386a76eda25b43e20aeb65f1d29b
YRZrandir <798245456@qq.com>
2019-11-30 11:43:55 +0800

    TestLogin

--- campusMarket/src/main/java/controller/UserController.java
+++ campusMarket/src/main/java/controller/UserController.java
@@ -15,6 +15,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import model.user.*;
 
@@ -26,14 +27,14 @@ public class UserController {
 	private ApplicationContext context;
 	
 	@CrossOrigin(origins = "*")
-	@RequestMapping(value="./login", method=RequestMethod.POST)
+	@RequestMapping(value="/login", method=RequestMethod.POST)
 	public void login(HttpServletRequest request, HttpServletResponse response) {
 		try {
 			request.setCharacterEncoding("UTF-8");
 			response.setCharacterEncoding("UTF-8");
-			
-			String id = request.getParameter("id");
-			String password = request.getParameter("password");
+			String id = (String)request.getParameter("id");
+			String password = (String)request.getParameter("password");
+			System.out.println("POST:" + id + password);
 			context = new ClassPathXmlApplicationContext("Beans.xml");
 			
 			UserDAO userDAO = context.getBean("UserJDBCTemplate", UserJDBCTemplate.class);
@@ -46,13 +47,13 @@ public class UserController {
 	}
 	
 	@CrossOrigin(origins = "*")
-	@RequestMapping(value="./register", method=RequestMethod.POST)
+	@RequestMapping(value="/register", method=RequestMethod.POST)
 	public void register(HttpServletRequest request, HttpServletResponse response) {
 		
 	}
 	
 	@CrossOrigin(origins = "*")
-	@RequestMapping(value="./updateUser", method=RequestMethod.POST)
+	@RequestMapping(value="/updateUser", method=RequestMethod.POST)
 	public void updateUser(HttpServletRequest request, HttpServletResponse response) {
 		
 	}
